5f5f695c169e2433a6a2ff8f91d688667ccc0cc3
bin <bh.huang@ieee.org>
2025-10-11 04:05:17 +0100

    Merge submodule content into main repository

---
+++ setup.py
@@ -0,0 +1,73 @@
+from setuptools import find_packages, setup, Extension
+import pkgconfig
+from pathlib import Path
+import numpy as np
+
+
+pkgconfig_result = pkgconfig.parse('libavformat libswscale opencv4')
+
+print("Numpy dir: ", np.get_include())
+
+mvextractor = Extension('mvextractor.videocap',
+    include_dirs = [
+        *pkgconfig_result['include_dirs'],
+        np.get_include()
+    ],
+    library_dirs = pkgconfig_result['library_dirs'],
+    libraries = pkgconfig_result['libraries'],
+    sources = [
+        'src/mvextractor/py_video_cap.cpp',
+        'src/mvextractor/video_cap.cpp',
+        'src/mvextractor/time_cvt.cpp',
+        'src/mvextractor/mat_to_ndarray.cpp'
+    ],
+    extra_compile_args = ['-std=c++11'],
+    extra_link_args = ['-fPIC', '-Wl,-Bsymbolic'])
+
+setup(
+    name='motion-vector-extractor',
+    author='Lukas Bommes',
+    author_email=' ',
+    version="1.1.0",
+    license='MIT',
+    url='https://github.com/LukasBommes/mv-extractor',
+    description=('Reads video frames and MPEG-4/H.264 motion vectors.'),
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: X11 Applications",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Multimedia :: Video :: Capture",
+        "Topic :: Multimedia :: Video :: Display",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: C",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    keywords=['motion vector', 'video capture', 'mpeg4', 'h.264', 'compressed domain'],
+    ext_modules=[mvextractor],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'extract_mvs=mvextractor.__main__:main',
+        ],
+    },
+    python_requires='>=3.9, <4',
+    # minimum versions of numpy and opencv are the oldest versions
+    # just supporting the minimum Python version (Python 3.9)
+    install_requires=['numpy>=1.19.3', 'opencv-python>=4.4.0.46']
+)
---
+++ tests/end_to_end_tests.py
@@ -0,0 +1,87 @@
+import os
+import time
+import tempfile
+import unittest
+import subprocess
+
+import cv2
+import numpy as np
+
+
+PROJECT_ROOT = os.getenv("PROJECT_ROOT", "")
+
+
+class TestEndToEnd(unittest.TestCase):
+
+    def motions_vectors_valid(self, outdir, refdir):
+        equal = []
+        num_mvs = len(os.listdir(os.path.join(refdir, "motion_vectors")))
+        for i in range(num_mvs):
+            mvs = np.load(os.path.join(outdir, "motion_vectors", f"mvs-{i}.npy"))
+            mvs_ref = np.load(os.path.join(refdir, "motion_vectors", f"mvs-{i}.npy"))
+            equal.append(np.all(mvs == mvs_ref))
+        return all(equal)
+
+
+    def frame_types_valid(self, outdir, refdir):
+        with open(os.path.join(outdir, "frame_types.txt"), "r") as file:
+            frame_types = [line.strip() for line in file]
+        with open(os.path.join(refdir, "frame_types.txt"), "r") as file:
+            frame_types_ref = [line.strip() for line in file]
+        return frame_types == frame_types_ref
+
+
+    def frames_valid(self, outdir, refdir):
+        equal = []
+        num_frames = len(os.listdir(os.path.join(refdir, "frames")))
+        for i in range(num_frames):
+            frame = cv2.imread(os.path.join(outdir, "frames", f"frame-{i}.jpg"))
+            frame_ref = cv2.imread(os.path.join(refdir, "frames", f"frame-{i}.jpg"))
+            equal.append(np.all(frame == frame_ref))
+        return all(equal)
+
+
+    def test_end_to_end_h264(self):
+        with tempfile.TemporaryDirectory() as outdir:
+            print("Running extraction for H.264")
+            subprocess.run(f"extract_mvs {os.path.join(PROJECT_ROOT, 'vid_h264.mp4')} --dump {outdir}", shell=True, check=True)
+            refdir = os.path.join(PROJECT_ROOT, "tests/reference/h264")
+
+            self.assertTrue(self.motions_vectors_valid(outdir, refdir), msg="motion vectors are invalid")
+            self.assertTrue(self.frame_types_valid(outdir, refdir), msg="frame types are invalid")
+            self.assertTrue(self.frames_valid(outdir, refdir), msg="frames are invalid")
+
+
+    def test_end_to_end_mpeg4_part2(self):
+        with tempfile.TemporaryDirectory() as outdir:
+            print("Running extraction for MPEG-4 Part 2")
+            subprocess.run(f"extract_mvs {os.path.join(PROJECT_ROOT, 'vid_mpeg4_part2.mp4')} --dump {outdir}", shell=True, check=True)
+            refdir = os.path.join(PROJECT_ROOT, "tests/reference/mpeg4_part2")
+
+            self.assertTrue(self.motions_vectors_valid(outdir, refdir), msg="motion vectors are invalid")
+            self.assertTrue(self.frame_types_valid(outdir, refdir), msg="frame types are invalid")
+            self.assertTrue(self.frames_valid(outdir, refdir), msg="frames are invalid")
+
+
+    def test_end_to_end_rtsp(self):
+        with tempfile.TemporaryDirectory() as outdir:
+            print("Setting up end to end test for RTSP")
+            media_server_binary = os.path.abspath(os.path.join(PROJECT_ROOT, "tests/tools/live555MediaServer"))
+            rtsp_server = subprocess.Popen(media_server_binary, cwd=PROJECT_ROOT if PROJECT_ROOT else None)
+            try:
+                time.sleep(1)
+                print("Running extraction for RTSP stream")
+                rtsp_url = "rtsp://localhost:554/vid_h264.264"
+                subprocess.run(f"extract_mvs {rtsp_url} --dump {outdir}", shell=True, check=True)
+                refdir = os.path.join(PROJECT_ROOT, "tests/reference/rtsp")
+
+                self.assertTrue(self.motions_vectors_valid(outdir, refdir), msg="motion vectors are invalid")
+                self.assertTrue(self.frame_types_valid(outdir, refdir), msg="frame types are invalid")
+                self.assertTrue(self.frames_valid(outdir, refdir), msg="frames are invalid")
+            finally:
+                rtsp_server.terminate()
+
+
+if __name__ == '__main__':
+    unittest.main()
+            
